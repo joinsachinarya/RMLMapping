bbf603572492e4b5892698933db4a0cdb41ff7bd
sac6inarya <sachinkarya333@gmail.com>
2023-04-10 11:31:55 +0530

    first

---
+++ src/exe.ts
@@ -0,0 +1,22 @@
+import * as RmlParser from '@comake/rmlmapper-js';
+import { mappingSpecification } from './mapping-specifications';
+import { inputData } from './mapping-file';
+
+import fs from "fs";
+import path from 'path';
+const fsPromise = fs.promises;
+const myFile = path.join(__dirname, "../src/output.txt");
+
+const parsingOptions = {
+  toRDF: true,
+  replace: true
+}
+
+const createFile = async (data: unknown) => {
+  await fsPromise.appendFile(myFile, JSON.stringify(data) + "\n");
+}
+const solve = async () => {
+  const mappedFile = await RmlParser.parseTurtle(mappingSpecification, inputData, parsingOptions);
+  createFile(mappedFile);
+}
+solve();
