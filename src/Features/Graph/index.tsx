6b33de581c6ed24057d9b296d4ef1f5362073c94
sac6inarya <sachinkarya333@gmail.com>
2023-04-14 12:22:07 +0530

    new baranch

--- src/Features/Graph/index.tsx
+++ src/Features/Graph/index.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { useRef, useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
 import { GraphView } from "react-digraph";
 import { sample, getServerMonitoring, getUpdatedData } from "./sample";
 import {
@@ -9,70 +9,63 @@ import {
   SERVER_TYPE,
 } from "./config";
 
-class Graph extends Component {
-  constructor(props) {
-    super(props);
-    this.customNodeRef = React.createRef();
-    this.state = {
-      graph: sample,
-      selected: {},
-    };
-  }
+function Graph() {
+  const customNodeRef = useRef(null);
+  const [graph, setGraph] = useState(sample);
+  const [selected, setSelected] = useState({});
 
-  componentDidMount() {
+  useEffect(() => {
     getServerMonitoring().then((response) =>
-      this.setState({ graph: getUpdatedData(response) })
+      setGraph(getUpdatedData(response))
     );
-  }
+  }, []);
 
-  getNodeIndex(searchNode) {
-    return this.state.graph.nodes.findIndex((node) => {
+  function getNodeIndex(searchNode) {
+    return graph.nodes.findIndex((node) => {
       return node[NODE_KEY] === searchNode[NODE_KEY];
     });
   }
 
-  // Helper to find the index of a given edge
-  getEdgeIndex(searchEdge) {
-    return this.state.graph.edges.findIndex((edge) => {
+  function getEdgeIndex(searchEdge) {
+    return graph.edges.findIndex((edge) => {
       return (
         edge.source === searchEdge.source && edge.target === searchEdge.target
       );
     });
   }
 
-  // Given a nodeKey, return the corresponding node
-  getViewNode(nodeKey) {
+  function getViewNode(nodeKey) {
     const searchNode = {};
     searchNode[NODE_KEY] = nodeKey;
-    const i = this.getNodeIndex(searchNode);
-    return this.state.graph.nodes[i];
+    const i = getNodeIndex(searchNode);
+    return graph.nodes[i];
   }
 
-  deleteStartNode = () => {
-    const graph = this.state.graph;
-    graph.nodes.splice(0, 1);
-    graph.nodes = [...this.state.graph.nodes];
-    this.setState({ graph });
-  };
+  function deleteStartNode() {
+    const newGraph = { ...graph };
+    newGraph.nodes.splice(0, 1);
+    newGraph.nodes = [...graph.nodes];
+    setGraph(newGraph);
+  }
 
-  onUpdateNode = (viewNode) => {
-    const graph = this.state.graph;
-    const i = this.getNodeIndex(viewNode);
+  function onUpdateNode(viewNode) {
+    const newGraph = { ...graph };
+    const i = getNodeIndex(viewNode);
 
-    graph.nodes[i] = viewNode;
-    this.setState({ graph });
-  };
+    newGraph.nodes[i] = viewNode;
+    setGraph(newGraph);
+  }
 
-  onSelectNode = (viewNode, event) => {
-    this.setState({ selected: viewNode });
-  };
+  function onSelectNode(viewNode, event) {
+    setSelected(viewNode);
+  }
 
-  onSelectEdge = (viewEdge) => {
-    this.setState({ selected: viewEdge });
-  };
+  function onSelectEdge(viewEdge) {
+    setSelected(viewEdge);
+  }
 
-  onCreateNode = (x, y) => {
-    const graph = this.state.graph;
+  function onCreateNode(x, y) {
+    const newGraph = { ...graph };
     const type = Math.random() < 0.25 ? SERVER_TYPE : CUSTOM_EMPTY_TYPE;
 
     const viewNode = {
@@ -83,60 +76,55 @@ class Graph extends Component {
       y,
     };
 
-    graph.nodes = [...graph.nodes, viewNode];
-    this.setState({ graph });
-  };
+    newGraph.nodes = [...graph.nodes, viewNode];
+    setGraph(newGraph);
+  }
 
-  onDeleteNode = (viewNode, nodeId, nodeArr) => {
-    const graph = this.state.graph;
-    // Delete any connected edges
+  function onDeleteNode(viewNode, nodeId, nodeArr) {
+    const newGraph = { ...graph };
     const newEdges = graph.edges.filter((edge, i) => {
       return (
         edge.source !== viewNode[NODE_KEY] && edge.target !== viewNode[NODE_KEY]
       );
     });
 
-    graph.nodes = nodeArr;
-    graph.edges = newEdges;
+    newGraph.nodes = nodeArr;
+    newGraph.edges = newEdges;
 
-    this.setState({ graph, selected: null });
-  };
+    setGraph(newGraph);
+    setSelected(null);
+  }
 
-  // Called when an edge is reattached to a different target.
-  onSwapEdge = (sourceViewNode, targetViewNode, viewEdge) => {
-    const graph = this.state.graph;
-    const i = this.getEdgeIndex(viewEdge);
+  function onSwapEdge(sourceViewNode, targetViewNode, viewEdge) {
+    const newGraph = { ...graph };
+    const i = getEdgeIndex(viewEdge);
     const edge = JSON.parse(JSON.stringify(graph.edges[i]));
 
     edge.source = sourceViewNode[NODE_KEY];
     edge.target = targetViewNode[NODE_KEY];
-    graph.edges[i] = edge;
-    // reassign the array reference if you want the graph to re-render a swapped edge
-    graph.edges = [...graph.edges];
+    newGraph.edges[i] = edge;
+    newGraph.edges = [...graph.edges];
 
-    this.setState({
-      graph,
-      selected: edge,
-    });
-  };
+    setGraph(newGraph);
+    setSelected(edge);
+  }
 
-  onDeleteEdge = (viewEdge, edges) => {
-    const { graph } = this.state;
-    graph.edges = edges;
-    this.setState({ graph, selected: null });
-  };
+  function onDeleteEdge(viewEdge, edges) {
+    const newGraph = { ...graph };
+    newGraph.edges = edges;
+    setGraph(newGraph);
+    setSelected(null);
+  }
 
-  render() {
-    const { graph, selected } = this.state;
-
-    return (
+  return (
+    <>
       <div id="graph" style={{ height: "50rem" }}>
         <GraphView
           showGraphControls={true}
-          gridSize="100rem"
+          gridSize={100}
           gridDotSize={1}
           renderNodeText={false}
-          ref="GraphView"
+          ref={customNodeRef}
           nodeKey={NODE_KEY}
           nodes={graph.nodes}
           edges={graph.edges}
@@ -144,17 +132,18 @@ class Graph extends Component {
           nodeTypes={nodeConfig.NodeTypes}
           nodeSubtypes
           edgeTypes={nodeConfig.EdgeTypes}
-          onSelectNode={this.onSelectNode}
-          onCreateNode={this.onCreateNode}
-          onUpdateNode={this.onUpdateNode}
-          onDeleteNode={this.onDeleteNode}
-          onSelectEdge={this.onSelectEdge}
-          onSwapEdge={this.onSwapEdge}
-          onDeleteEdge={this.onDeleteEdge}
+          onSelectNode={onSelectNode}
+          onCreateNode={onCreateNode}
+          onUpdateNode={onUpdateNode}
+          onDeleteNode={onDeleteNode}
+          onSelectEdge={onSelectEdge}
+          onSwapEdge={onSwapEdge}
+          onDeleteEdge={onDeleteEdge}
           readOnly
         />
       </div>
-    );
-  }
+    </>
+  );
 }
+
 export default Graph;
