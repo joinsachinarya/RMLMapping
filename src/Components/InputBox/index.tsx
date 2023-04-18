1d0143979020ba7cfa4bcf63a3eecee52f69483c
sac6inarya <sachinkarya333@gmail.com>
2023-04-18 12:20:25 +0530

    input data moved to assets

--- src/Components/InputBox/index.tsx
+++ src/Components/InputBox/index.tsx
@@ -6,6 +6,7 @@ const InputBox = () => {
     <>
       <div className="flex flex-col w-1/4 h-screen border-solid border-t-2 border-r-2 border-gray-300 bg-gray-100  input-box-container">
         <Tools />
+
         <InputDataPreview />
       </div>
     </>
---
+++ src/Features/InputDataBox/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InputDataBox() {
+  return <div>InputDataBox</div>;
+}
+
+export default InputDataBox;
